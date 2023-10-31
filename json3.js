f14b5754a0df447ea88f946794f1953f6ea1de53
Elio <elio.sanfratello@gmail.com>
2023-10-31 10:10:37 +0100

    Json3 modifiche

--- json3.js
+++ json3.js
@@ -9,17 +9,16 @@ const person1 = {
     lastName: 'Rossi',
     age: 25,
     address: {
-      state: 'Italy',
-      city: 'Rome',
-      street: 'Via Romano, 12'
+        state: 'Italy',
+        city: 'Rome',
+        street: 'Via Romano, 12'
     }
-  };
-  
-  const person2 = JSON.stringify(person1);
-  newPerson2 = JSON.parse(person2)
-  newPerson2.address.city = 'Milan';
-  
-  console.log(person1);
-  console.log(newPerson2)
+};
+
+const person2 = JSON.parse(JSON.stringify(person1))
+person2.address.city = 'Milan';
+
+console.log(person1);
+console.log(person2)
 
 
